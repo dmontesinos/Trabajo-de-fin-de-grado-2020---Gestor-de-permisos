944cd8c98ad0ae32c3ba038ccbed46a2cb05d7fd
daniman5 <danibasquetmania@hotmail.com>
2020-12-11 13:37:06 +0100

    Laravel instalado y primera petición satisfactoria a la API de "mostrar todos los profesores"

---
+++ api/laravel/database/factories/ProfesoresFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Profesores;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProfesoresFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Profesores::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
