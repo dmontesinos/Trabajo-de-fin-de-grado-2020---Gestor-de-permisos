72f4f26e9d898367fdf6b36cc3a654fd0d404fd2
daniman5 <danibasquetmania@hotmail.com>
2020-12-26 18:50:08 +0100

    Nuevas peticiones para la API
    - Búsqueda de objetos por ID, nombre o descripción.
    - Eliminación de objetos por ID.

--- api/laravel/app/Http/Controllers/ObjetoController.php
+++ api/laravel/app/Http/Controllers/ObjetoController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Objeto;
 use Illuminate\Support\Facades\DB;
 
 
 class ObjetoController extends Controller
 {
+    //Añade un objeto nuevo a la base de datos.
     function addObjeto(Request $req){
         if($req->input('nom') && $req->input('descripcio')){
             $id = DB::table('Objeto')->insertGetId( //Mediante inserGetId sacamos el id auto-incremental después de añadirlo.
@@ -25,5 +25,44 @@ class ObjetoController extends Controller
         } else {
             return ["resultat" => "L'objecte no s'ha pogut afegir a la base de dades."];
         }
+    }
+
+    //Búsqueda de objetos por ID
+    function searchObjetoID($busqueda){
+        if($busqueda){
+            return DB::table('Objeto')->where('idObjeto', $busqueda)->get();
+        } else {
+            return ["resultat" => "L'objecte no s'ha trobat a la base de dades."];
+        }
+    }
+
+    //Búsqueda de objetos por nombre
+    function searchObjetoNombre($busqueda){
+        if($busqueda){
+            return DB::table('Objeto')->where('nombre', 'like', '%'.$busqueda.'%')->get();
+        } else {
+            return ["resultat" => "L'objecte no s'ha trobat a la base de dades."];
+        }
+    }
+
+    //Busca en la DB los objetos que contengan en la descripción el String dado.
+    function searchObjetoDesc($busqueda){
+        if($busqueda){
+            return DB::table('Objeto')->where('descripcion', 'like', '%'.$busqueda.'%')->get();
+        } else {
+            return ["resultat" => "L'objecte no s'ha trobat a la base de dades."];
+        }
+    }    
+
+
+    function eliminarObjeto($id){
+        if($id){
+            DB::table('Permisos')->where('Objeto_idObjeto', $id)->delete();
+            DB::table('Objeto')->where('idObjeto', $id)->delete();
+            return["resultat"=>"Objecte amb id $id eliminar correctament."];
+        } else {
+            return["resultat"=>"L'objecte no s'ha pogut eliminar."];
+        }
+        
     }
 }
--- api/laravel/routes/api.php
+++ api/laravel/routes/api.php
@@ -21,9 +21,14 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//Route::apiResource("profesores", "ProfesoresController");
+// Profesores
+Route::get('professors/{niu}', [ProfesoresController::class, 'profesor']); //Retorna info profesor
+Route::get('professors/carrecs/{niu}', [ProfesoresController::class, 'cargos']); //Retorna cargos de un profesor
+Route::get('professors/departaments/{niu}', [ProfesoresController::class, 'departamentos']); //Retorna departamentos de un profesor
 
-Route::get('professors/{niu}', [ProfesoresController::class, 'profesor']);
-Route::get('professors/carrecs/{niu}', [ProfesoresController::class, 'cargos']);
-Route::get('professors/departaments/{niu}', [ProfesoresController::class, 'departamentos']);
-Route::post('objectes/afegir/', [ObjetoController::class, 'addObjeto']);
+// Objetos
+Route::post('objectes/afegir/', [ObjetoController::class, 'addObjeto']); //Añade un objeto nuevo a la DB
+Route::get('objectes/cercar/id/{id}', [ObjetoController::class, 'searchObjetoID']); //Busca por ID de objeto
+Route::get('objectes/cercar/nom/{nom}', [ObjetoController::class, 'searchObjetoNombre']); //Busca por nombre de objeto
+Route::get('objectes/cercar/descripcio/{descripcio}', [ObjetoController::class, 'searchObjetoDesc']); //Busca objetos que coincidan con la búsqueda
+Route::delete('objectes/eliminar/{id}', [ObjetoController::class, 'eliminarObjeto']); //Elimina el objeto por ID
